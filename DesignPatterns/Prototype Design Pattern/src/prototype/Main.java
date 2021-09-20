31e0c179fb27b92cc3be35b4db89b313ab7ba453
ushara09 <thimalushara@gmail.com>
2021-09-20 09:16:09 +0530

    prototype pattern done

---
+++ DesignPatterns/Prototype Design Pattern/src/prototype/Main.java
@@ -0,0 +1,33 @@
+package prototype;
+
+class Person implements Cloneable {
+    public String firstName;
+    public String lastName;
+
+    @Override
+    protected Person clone() throws CloneNotSupportedException {
+        return (Person) super.clone();
+    }
+}
+
+public class Main {
+    public static void main(String args[]){
+
+        Person p1 = new Person();
+        p1.firstName = "Ushara";
+        p1.lastName = "Geekiyanage";
+
+        try {
+            //cloning the p1 object to p2
+            Person p2 = p1.clone();
+
+            //cloned object
+            System.out.println("First name - "+p2.firstName);
+            System.out.println("Last name - "+p2.lastName);
+            
+        }catch (CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+
+    }
+}
