2be15b3828b992ba65ed62c20c9c647835229659
Thimal Ushara <thimalushara@gmail.com>
2022-11-06 12:00:04 +0530

    RunningSumof1dArray

--- Leetcode/src/RunningSumof1dArray.java
+++ Leetcode/src/RunningSumof1dArray.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+
 public class RunningSumof1dArray {
+    public static void main(String[] args) {
+
+        int[] nums = {3,1,2,10,1};
+        RunningSumof1dArray obj = new RunningSumof1dArray();
+        obj.arrSum(nums);
+
+    }
+    public int[] arrSum(int[] nums){
+
+        int[] tempArr = nums;
+        int[] summedArr = new int[nums.length];
+        int total = 0;
+        for(int i = 0; i < nums.length; i++){
+
+            summedArr[i] = tempArr[i] + total;
+            total = summedArr[i];
+        }
+
+        System.out.println(Arrays.toString(summedArr));
+        return summedArr;
+    }
 }
